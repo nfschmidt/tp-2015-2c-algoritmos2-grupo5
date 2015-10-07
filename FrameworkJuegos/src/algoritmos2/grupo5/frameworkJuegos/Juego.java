4748a09987dc16a0ee556917d95a2dcf547599d7
mariaguadalupeuvia <mariaguadalupeuvia@gmail.com>
2015-10-06 22:06:17 -0300

    convierto la clase Juego en abstracta y agrego algunos metodos

--- FrameworkJuegos/src/algoritmos2/grupo5/frameworkJuegos/Juego.java
+++ FrameworkJuegos/src/algoritmos2/grupo5/frameworkJuegos/Juego.java
@@ -1,6 +1,20 @@
 package algoritmos2.grupo5.frameworkJuegos;
 
-public class Juego {
-
+public abstract class Juego {
+	public abstract String getCategoria();//la categoria me permite agrupar por tipo de juego, para poder armar un menu por ejemplo
+	public abstract String getNombre();//nombre del juego que se ve en el menu
+	public abstract String getDescripcion();
+	public abstract void inicializar();
+	public abstract void cerrar();
+	public abstract void jugar();//este seria el que llama a las jugadas
+	private String dirJuego; //path del directorio donde se ubican los recursos del juego en caso de tener alguno
+	public string getDirJuego()
+	{
+		return dirJuego;
+	}
+    public void setDirJuego(String path)
+    {
+    	this.dirJuego = path;
+    }
 }
 //Comentario de prueba
